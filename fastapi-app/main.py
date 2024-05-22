c626da233c025329ab89441c2d0b9c96b073593b
Vladuslav Dragushin <maxhc@inbox.ru>
2024-05-23 00:27:42 +0300

    Added first app

---
+++ fastapi-app/main.py
@@ -0,0 +1,20 @@
+from core.config import settings
+
+import uvicorn
+
+from fastapi import FastAPI
+from api import router as api_router
+
+app = FastAPI()
+app.include_router(
+    api_router,
+    prefix=settings.api.prefix
+)
+
+if __name__ == '__main__':
+    uvicorn.run(
+        "main:app",
+        host=settings.run.host,
+        port=settings.run.port,
+        reload=settings.run.reload,
+    )
